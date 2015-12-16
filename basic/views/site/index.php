4ed7050b369b8fb85f8efc6d7e390e468eb47441
chankay <chankay@aliyun.com>
2015-12-16 16:24:51 +0800

    null

--- basic/views/site/index.php
+++ basic/views/site/index.php
@@ -21,17 +21,14 @@
             width: 80%;
             margin-left: 10%;
         }
-        .list-group-item{
-            border: none;
-        }
         .list-group-item a{
-            color: #5639A3;
+            color: #7D78CD;;
         }
-        .col-sm-3 img{
+/*        .col-sm-3 img{
             width:80%;
             height: 80%;
             margin-left: 5%;
-        }
+        }*/
 
 
         @media (min-width: 1280px) {
@@ -177,11 +174,11 @@
             <div class="col-sm-3 col-lg-3">
                 <img src="/images/police3.gif"/>
                 <ul class="list-group">
-                    <li class="list-group-item"><a href="/site/jdxz">鉴定须知</a></li>
-                    <li class="list-group-item"><a href="/site/lct">伤情鉴定流程图</a></li>
-                    <li class="list-group-item"><a href="/site/yysy">常用医用术语释义</a></li>
-                    <li class="list-group-item"><a href="/site/jdbz">人体损伤程度鉴定标准</a></li>
-                    <li class="list-group-item"><a href="/site/tsss">特殊损伤证据收集注意事项</a></li>
+                    <li class="list-group-item"><img src="/images/list.png" style="width: 10%;height: 10%;"><a href="/site/jdxz">鉴定须知</a></li>
+                    <li class="list-group-item"><img src="/images/list.png" style="width: 10%;height: 10%;"><a href="/site/lct">伤情鉴定流程图</a></li>
+                    <li class="list-group-item"><img src="/images/list.png" style="width: 10%;height: 10%;"><a href="/site/yysy">常用医用术语释义</a></li>
+                    <li class="list-group-item"><img src="/images/list.png" style="width: 10%;height: 10%;"><a href="/site/jdbz">人体损伤程度鉴定标准</a></li>
+                    <li class="list-group-item"><img src="/images/list.png" style="width: 10%;height: 10%;"><a href="/site/tsss">特殊损伤证据收集注意事项</a></li>
                 </ul>
                 <!--<img src="police.jpg" style="width: 270px; height: 500px;">-->
             </div>
