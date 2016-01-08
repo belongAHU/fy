46d2f952df0f8cb14fc0b854e439671425638b92
nbamboo <nbamboo@hotmail.com>
2016-01-08 10:38:49 +0800

    null

--- basic/views/site/jdbz.php
+++ basic/views/site/jdbz.php
@@ -646,10 +646,6 @@
     </div>
 </div>
 </div>
-<hr class="featurette-divider">
-
-
-
 <script src="./bootstrap-3.3.5-dist/jquery-2.1.4/jquery.min.js"></script>
 <script src="./bootstrap-3.3.5-dist/js/bootstrap.min.js"></script>
 </body>
